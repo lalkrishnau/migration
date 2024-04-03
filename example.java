7febac4d3e96ecdbedf434cd0c60c61a2ed4dd96
lalkrishnau <151121482+lalkrishnau@users.noreply.github.com>
2024-04-03 12:50:07 +0530

    Create example.java

---
+++ example.java
@@ -0,0 +1,9 @@
+public class StudentDTO {
+    
+    private int rollNo;
+    private String name;
+    
+    // constructors
+    // getters & setters
+    // equals(), hashcode() & toString() methods
+}
