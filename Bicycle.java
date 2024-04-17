cc32cf04b42cc6f316c5810afc6cdbd44ff87257
lalkrishnau <151121482+lalkrishnau@users.noreply.github.com>
2024-04-17 11:38:15 +0530

    Update and rename addition.java to Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,10 @@
+public final class Bicycle extends Vehicle {
+    String type;
+    Long wheelSize;
+
+    public Bicycle(String code, String engineType, String type, Long wheelSize) {
+        super(code, engineType);
+        this.type = type;
+        this.wheelSize = wheelSize;
+    }
+}
